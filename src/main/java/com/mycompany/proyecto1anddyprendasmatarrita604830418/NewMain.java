234e31bb9c60f3883cc7630e1d8af42b06370eb7
Anddy-Prendas-Matarrita <anddyprendasmatarrita@gmail.com>
2025-03-07 12:21:42 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/proyecto1anddyprendasmatarrita604830418/NewMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyecto1anddyprendasmatarrita604830418;
+
+/**
+ *
+ * @author Anddy Prendas
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.mycompany.proyecto1anddyprendasmatarrita604830418 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.proyecto1anddyprendasmatarrita604830418 to javafx.fxml;
+    exports com.mycompany.proyecto1anddyprendasmatarrita604830418;
+}
